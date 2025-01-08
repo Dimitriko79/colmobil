d92269ca50e515d656e665c7c9602d40ae8050dc
Dimitry <dimitriko@elad.co.il>
2025-01-08 16:28:52 +0200

    update errors

--- src/components/ChatBot/useChatBot.js
+++ src/components/ChatBot/useChatBot.js
@@ -47,7 +47,7 @@ export const useChatBot = () => {
                 user_id: user_id
             };
             const result = await axios.post(
-                'https://es46up9wxe.execute-api.us-east-1.amazonaws.com/test/main_chat',
+                'https://es46up9wxe.execute-api.us-east-1.amazonaws.com/test',
                 requestBody,
                 {
                     headers: {
