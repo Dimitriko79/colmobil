aeef11cfbd3baab093e4460046b98b29c6c7013f
Dimitry <dimitriko@elad.co.il>
2025-01-08 13:32:06 +0200

    test

--- src/components/ChatBot/useChatBot.js
+++ src/components/ChatBot/useChatBot.js
@@ -47,7 +47,7 @@ export const useChatBot = () => {
                 user_id: user_id
             };
             const result = await axios.post(
-                'http://54.158.73.214:5001/main_chat',
+                'https://a2iutytjgb.execute-api.us-east-1.amazonaws.com/main_chat',
                 requestBody,
                 {
                     headers: {
